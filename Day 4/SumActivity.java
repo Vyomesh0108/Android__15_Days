cf9bc48b3112c07ead605a2f532247f3acd6ef61
Vyomesh Mistry <84772530+Vyomesh0108@users.noreply.github.com>
2021-05-29 13:26:06 +0530

    Day 4 Android : Print Sum of 2 numbers
    Print Sum of 2 Numbers in Android
    Code & Output Screenshot

---
+++ Day 4/SumActivity.java
@@ -0,0 +1,47 @@
+package com.example.mydemoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SumActivity extends AppCompatActivity {
+EditText etNum1,etNum2;
+TextView tvans;
+Button btnsubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sum);
+
+        etNum1=findViewById(R.id.etNum1);
+        etNum2=findViewById(R.id.etNum2);
+        tvans=findViewById(R.id.tvans);
+        btnsubmit=findViewById(R.id.btnsubmit);
+
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n1,n2;
+                n1 = etNum1.getText().toString();
+                n2 = etNum2.getText().toString();
+
+                int num1,num2,sum;
+
+                num1 = Integer.parseInt(n1);
+                num2 = Integer.parseInt(n2);
+
+                sum = num1+num2;
+
+                tvans.setText("Ans is"+sum);
+
+                Toast.makeText(SumActivity.this, "Ans is"+sum, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
